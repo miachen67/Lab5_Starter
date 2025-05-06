571bf8f8c1b03114b6cde3093a9462e6a5b3af73
miachen67 <miachen2005@gmail.com>
2025-05-05 19:17:50 -0700

    unit tests done

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Lab 5 - Starter
 Make sure you make a PR to your own repo's main and not the class' repo!! Otherwise you will lose points!!
 Mia Chen, Lucas Hlaing
+
+
+## Check Your Understanding
+
+1. No, sending a message involves too many components of the application so unit testing is not a reliable form of testing.
+2. Yes, this is a simple and small feature to test. 
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -9,3 +9,73 @@ import {
 } from '../code-to-unit-test/unit-test-me';
 
 // TODO - Part 2
+
+// Test Valid Phone Numbers
+test('test 12345678910', () => {
+    expect(isPhoneNumber(12345678910)).toBe(false);
+});
+test('test 123aaa3334', () => {
+    expect(isPhoneNumber('123aaa3334')).toBe(false);
+});
+test('test 123-000-3334', () => {
+    expect(isPhoneNumber('123-000-3334')).toBe(true);
+});
+test('test 122-111-0000', () => {
+    expect(isPhoneNumber('123-000-3334')).toBe(true);
+});
+
+// Test Valid email
+test('test sth@ucsd.edu', () => {
+  expect(isEmail("sth@ucsd.edu")).toBe(true);
+});
+test('test hihi@gmail.com', () => {
+  expect(isEmail("hihi@gmail.com")).toBe(true);
+});
+test('test null', () => {
+  expect(isEmail("")).toBe(false);
+});
+test('test hi@hi@ucsd.edu', () => {
+  expect(isEmail("hi@hi@ucsd.edu")).toBe(false);
+});
+
+// Test Date
+test('test 5/25/2025', () => {
+  expect(isDate("5/25/2025")).toBe(true);
+});
+test('test 52/4/2025', () => {
+  expect(isDate("52/4/2025")).toBe(true);
+});
+test('test 2/4/25', () => {
+  expect(isDate("2/4/25")).toBe(false);
+});
+test('test 241/4/1125', () => {
+  expect(isDate("241/4/1125")).toBe(false);
+});
+
+// Test strong passwords
+test('test a34', () => {
+    expect(isStrongPassword('a34')).toBe(false);
+});
+test('test a34asjdklajfalksdjf123', () => {
+    expect(isStrongPassword('a34asjdklajfalksdjf123')).toBe(false);
+});
+test('test a3mf_', () => {
+    expect(isStrongPassword('a3mf_')).toBe(true);
+});
+test('test a493_6', () => {
+    expect(isStrongPassword('a493_6')).toBe(true);
+});
+
+// text valid HEX
+test('test a493_6', () => {
+    expect(isHexColor('a493_6')).toBe(false);
+});
+test('test 5ab5gba', () => {
+  expect(isHexColor('5ab5gba')).toBe(false);
+});
+test('test FFFFFF', () => {
+    expect(isHexColor('FFFFFF')).toBe(true);
+});
+test('test EFE', () => {
+    expect(isHexColor('EFE')).toBe(true);
+});
