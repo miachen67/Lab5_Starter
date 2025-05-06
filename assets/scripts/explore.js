003ec98fc0d06a1926537e5fe3914131e3b0421c
miachen67 <miachen2005@gmail.com>
2025-05-05 18:58:25 -0700

    Finished Explore

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,50 @@
 // explore.js
 
 window.addEventListener('DOMContentLoaded', init);
+let currentVoice = null
+let voices
 
 function init() {
-  // TODO
+  const synth = window.speechSynthesis
+  const voiceSelect = document.getElementById("voice-select")
+
+  synth.onvoiceschanged = () => {
+    voices = synth.getVoices();
+    
+      for (let i = 0; i < voices.length; i++){
+          const option = document.createElement("option")
+          option.value = voices[i].name
+          option.textContent = `${voices[i].name} (${voices[i].lang})`;
+          if (voices[i].default) {
+            option.textContent += " — DEFAULT";
+          }
+          voiceSelect.appendChild(option)
+      }
+  }
+  const talkButton = document.querySelector("button")
+  talkButton.addEventListener("click", (event) => talk(event, synth))
+  voiceSelect.addEventListener("change", (event) => changeVoice(event, voices))
 }
+
+function talk(event, synth) {
+  const inputs = document.getElementById("text-to-speak")
+  console.log(inputs)
+  let textBlurb = inputs.value
+  let utterance = new SpeechSynthesisUtterance(textBlurb);
+  utterance.voice = currentVoice;
+  const face = document.querySelector("img")
+  face.src = "assets/images/smiling-open.png"
+  synth.speak(utterance)
+  utterance.addEventListener("end", () => {
+    face.src = "assets/images/smiling.png"
+  })
+}
+
+function changeVoice(event, voices) {
+  for (let i = 0; i < voices.length; i++) {
+    if (voices[i].name === event.target.value) {
+      currentVoice = voices[i];
+    }
+  }
+}
+  
